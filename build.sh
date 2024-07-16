18869cff60a6eb8ac7701865fc0f060d1a5e48af
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-07-17 07:48:47 +1200

    Update build script to include virtual environment activation and binary packaging

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "deleting existing build...."
+
+
+rm -rf dist
+rm -rf build
+
+echo "Activating virtual environment...."
+
+source erpvenv/bin/activate
+
+echo "packaging binary / executable file...."
+pyinstaller erp-vis.spec
+
+echo "deactivating virtual environment...."
+deactivate
